b2e9d8503607d6a4ada5780db7d27cc906aff086
Prince Thind <Princethind799@gmail.com>
2022-03-24 14:54:36 +0530

    add basic userRegistration

---
+++ resources/views/user-form.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+<h1>Registration Page</hi>
+<form method="POST">
+    @csrf
+<input type="text" name="username" placeholder="Username" > <br> <br>
+<input type="text" name="password" placeholder="Password" > <br> <br>
+<button type="submit">Submit</button>
+
+@isset($error)
+    <div style="color:red">
+            {{ $error }}
+  </div>
+@endisset
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('user-form');
+});
+
+Route::post('/register', [RegisterController::class, "register"]);
+
